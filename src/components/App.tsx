3cd26a0ab87b48b188a0e2df9d3c60837b8f13e8
Ajinkya Deshpande <ajinkya.deshpande0000@gmail.com>
2022-03-07 02:41:07 +0530

    add react-router and create prettierrc

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,15 @@
+import { Routes, Route } from "react-router-dom";
+
+import Button from "@mui/material/Button";
+
+// Add a select for first time and then store it in local storage.
+
 const App = () => {
   return (
-    <div>
-      App
+    <div className="">
+      <Button variant="contained">Hello World</Button>
     </div>
   );
-}
+};
 
 export default App;
