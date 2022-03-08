f651e89d53ab9a6c7f3168db774f78638d9b852b
Ajinkya Deshpande <ajinkya.deshpande0000@gmail.com>
2022-03-08 19:26:31 +0530

    add local storage to persist data

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,9 +1,15 @@
-import { useEffect, useState } from "react";
+import { useState, FormEvent, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+// Material UI
 import OutlinedInput from "@mui/material/OutlinedInput";
 import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
+import FormHelperText from "@mui/material/FormHelperText";
 import Select, { SelectChangeEvent } from "@mui/material/Select";
+import Button from "@mui/material/Button";
+import SendIcon from "@mui/icons-material/Send";
 
 // Add a select for first time and then store it in local storage.
 
@@ -22,34 +28,64 @@ const names = ["Ajinkya", "Sushant", "Kashish", "Garvita"];
 
 const Home = () => {
   const [personName, setPersonName] = useState<string>("");
+  const [hasError, setHasError] = useState<boolean>(false);
+
+  const navigate = useNavigate();
 
   useEffect(() => {
-    console.log(personName);
-  }, [personName]);
+    const savedPersonName = localStorage.getItem("personName");
+    if (savedPersonName) {
+      navigate("/add-goal", { replace: true, state: savedPersonName });
+    }
+  }, [navigate]);
 
   const handleChange = (e: SelectChangeEvent<typeof personName>) => {
+    setHasError(false);
     setPersonName(e.target?.value);
+  };
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+
+    if (!personName) {
+      setHasError(true);
+      return;
+    }
+
+    localStorage.setItem("personName", personName);
+    navigate("/add-goal", { replace: true, state: personName });
   };
 
   return (
     <div className="home center">
-      <FormControl sx={{ m: 1, width: 300 }}>
-        <InputLabel id="select-name">Who are you?</InputLabel>
-        <Select
-          labelId="select-name"
-          id="select"
-          value={personName}
-          onChange={handleChange}
-          input={<OutlinedInput label="Who are you?" />}
-          MenuProps={MenuProps}
+      <form className="center name-form" onSubmit={handleSubmit}>
+        <FormControl sx={{ m: 1, width: 300 }} error={hasError}>
+          <InputLabel id="select-name">Who are you?</InputLabel>
+          <Select
+            labelId="select-name"
+            id="select"
+            value={personName}
+            onChange={handleChange}
+            input={<OutlinedInput label="Who are you?" />}
+            MenuProps={MenuProps}
+          >
+            {names.map((name) => (
+              <MenuItem key={name} value={name}>
+                {name}
+              </MenuItem>
+            ))}
+          </Select>
+          {hasError && <FormHelperText>Please select a name.</FormHelperText>}
+        </FormControl>
+        <Button
+          type="submit"
+          variant="contained"
+          endIcon={<SendIcon />}
+          sx={{ my: 2, width: 150 }}
         >
-          {names.map((name) => (
-            <MenuItem key={name} value={name}>
-              {name}
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
+          Add Goal
+        </Button>
+      </form>
     </div>
   );
 };
