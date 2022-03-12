3cd469a47fddc2b515fe041ecf1a8b4bcbf4f791
Ajinkya Deshpande <ajinkya.deshpande0000@gmail.com>
2022-03-12 12:59:24 +0530

    update template

--- api/postMessageToDiscord.js
+++ api/postMessageToDiscord.js
@@ -5,8 +5,8 @@ const goalTemplate = (templateData) => {
   const startDateFormat = new Date(startDate);
   const endDateFormat = new Date(endDate);
 
-  return `**Goal for the week [${startDateFormat.toLocaleDateString()} - ${endDateFormat.toLocaleDateString()}]**
-> By ${name.label}
+  return `Goal for the week **[${startDateFormat.toLocaleDateString()} - ${endDateFormat.toLocaleDateString()}]**
+> By **${name.label}**
 \`\`\`
 ${goalList.map((goal, index) => index + 1 + ". " + goal).join("\n")}
 \`\`\`
